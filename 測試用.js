da197394aceccd1ad888ae177d86d6d6d7105188
家銓\a0962 <a0962055671@gmail.com>
2025-04-02 10:42:58 +0800

    修改圖片移動距離 增加選取框的懸停效果

--- 測試用.js
+++ 測試用.js
@@ -1,11 +1,13 @@
 let currentIndex = 0; // 目前輪播的位置
 const totalImages = document.querySelectorAll('.character').length; // 總圖片數
 const visibleImages = 5; // 顯示的圖片數
-const imageWidth = 1000 + 10; // 圖片寬度 + 間距
 const maxIndex = totalImages - visibleImages; // 允許的最大索引
 
 function updateCarousel() {
     const characters = document.querySelector('.characters');
+    const character = document.querySelector('.character');
+    const width = character.offsetWidth;// 單張圖片寬度
+    const imageWidth = width + 10; // 單張圖片寬度 + 間距
     characters.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
 }
 
